15e8f04cb206ab0868687b28fd826f5daa7126db
Kévin Gossé <gossekevin1@gmail.com>
2016-12-30 22:46:45 +0100

    Add delete and edit icon

--- src/components/Fortune/Fortune.js
+++ src/components/Fortune/Fortune.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
-import { Card, Button, Badge} from 'antd';
+import { Card, Button, Badge, Icon} from 'antd';
 import './Fortune.css';
 
-const Fortune = (props) => {
-  const size = 'default';
-  return (
-    <div className="card">
-      <Card extra={<a href="#">More</a>}>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquid cum delectus doloribus, eos iste nesciunt nisi numquam optio pariatur placeat praesentium, quaerat quas quo reiciendis repellat sapiente sint.
-        </div>
-        <div>
-          <Button type="primary"  icon="dislike" size={size}><Badge count={25} /></Button>
-          <Button type="primary" icon="like" size={size}><Badge count={25} /></Button>
-        </div>
-      </Card>
-    </div>
-  )
-};
+class Fortune extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+
+  onMouseEnter() {
+    this.setState({hover: true});
+  }
+
+  onMouseLeave() {
+    this.setState({hover: false});
+  }
+
+  render() {
+    const size = 'default';
+
+    const extra = (
+      <div>
+        <Button type="ghost" shape="circle" icon="delete" />
+        <Button type="ghost" shape="circle" icon="edit" />
+      </div>
+    );
+    return (
+      <div className="card">
+        <Card extra={this.state.hover ? extra : null} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab aliquid cum delectus doloribus, eos iste nesciunt nisi numquam optio pariatur placeat praesentium, quaerat quas quo reiciendis repellat sapiente sint.
+          </div>
+          <div>
+            <Button type="primary"  icon="dislike" size={size}><Badge count={25} /></Button>
+            <Button type="primary" icon="like" size={size}><Badge count={25} /></Button>
+          </div>
+        </Card>
+      </div>
+    )
+  }
+}
 
 export default Fortune;
 
